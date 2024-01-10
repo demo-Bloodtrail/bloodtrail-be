e6421166b03159aba5641b88f3de1241b731c7fc
aeeazip <codnjs0809@naver.com>
2024-01-10 23:32:23 +0900

    deploy(#4) : dev CI 파이프라인 구축

---
+++ health.test.js
@@ -0,0 +1,14 @@
+import request from "supertest";
+import express from "express";
+import { healthRouter } from "./src/router/healthRouter.js"; // 경로를 실제 프로젝트 구조에 맞게 수정
+
+const app = express();
+app.use("/health", healthRouter);
+
+describe("Health Check API", () => {
+  it('should respond with "HELLO, I\'m Healthy!"', async () => {
+    const response = await request(app).get("/health");
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("HELLO, I'm Healthy!");
+  });
+});
