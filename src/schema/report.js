a2fc39097d34f10be47c8b2c717b9911b995ea36
Goosmos <bdt040321@gmail.com>
2024-01-11 13:35:01 +0900

    feat(#14) : 스키마 작성

---
+++ src/schema/report.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+
+const ReportSchema = new Schema({
+    type: {
+        type: (String, Schema.Types.ObjectId),
+        required: true,
+    },
+    claim_info: {
+        type: (['POST', 'CHAT'], Schema.Types.ObjectId),
+        required: true,
+    },
+    claimer: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        required: true,
+        defualt: Date.now,
+    }
+
+});
+
+const Report = mongoose.model("Report", ReportSchema);
+export default Report;
