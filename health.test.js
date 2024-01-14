791d88bf0775d5cc5041840d406212cf97242e17
aeeazip <codnjs0809@naver.com>
2024-01-15 02:33:31 +0900

    bug(#29) : deploy.yml 수정

--- health.test.js
+++ health.test.js
@@ -1,14 +1,17 @@
+import dotenv from "dotenv";
 import request from "supertest";
 import express from "express";
 import { healthRouter } from "./src/router/healthRouter.js"; // 경로를 실제 프로젝트 구조에 맞게 수정
+
+dotenv.config();
 
 const app = express();
 app.use("/health", healthRouter);
 
 describe("Health Check API", () => {
-  it('should respond with "HELLO, I\'m Healthy! NODE_ENV = dev"', async () => {
+  it('should respond with "HELLO, I\'m Healthy! REDIS_HOST = localhost"', async () => {
     const response = await request(app).get("/health");
     expect(response.status).toBe(200);
-    expect(response.text).toBe("HELLO, I'm Healthy! NODE_ENV = dev");
+    expect(response.text).toBe("HELLO, I'm Healthy! REDIS_HOST = localhost");
   });
 });
