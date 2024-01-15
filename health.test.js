2f25cc9bdd1b6707f11ec52b3a51d8681d336b31
aeeazip <codnjs0809@naver.com>
2024-01-16 01:02:43 +0900

    bug(#29) : health check 문장 수정

--- health.test.js
+++ health.test.js
@@ -9,9 +9,9 @@ const app = express();
 app.use("/health", healthRouter);
 
 describe("Health Check API", () => {
-  it('should respond with "HELLO, I\'m Healthy! REDIS_HOST = localhost"', async () => {
+  it('should respond with "HELLO, I\'m Healthy!"', async () => {
     const response = await request(app).get("/health");
     expect(response.status).toBe(200);
-    expect(response.text).toBe("HELLO, I'm Healthy! REDIS_HOST = localhost");
+    expect(response.text).toBe("HELLO, I'm Healthy!");
   });
 });
--- src/controller/healthController.js
+++ src/controller/healthController.js
@@ -3,5 +3,5 @@ import dotenv from "dotenv";
 dotenv.config(); // .env 파일 사용 (환경 변수 관리)
 
 export const healthCheck = (req, res, next) => {
-  res.send("HELLO, I'm Healthy! REDIS_HOST = " + process.env.REDIS_HOST);
+  res.send("HELLO, I'm Healthy!");
 };
