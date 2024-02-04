c8b4e3340286ccaefaf630a9a756b139586820e4
aeeazip <codnjs0809@naver.com>
2024-02-05 02:12:29 +0900

    feature(#110) : 지정헌혈글 전체 조회 uri 변경

--- src/router/bloodRouter.js
+++ src/router/bloodRouter.js
@@ -14,7 +14,7 @@ export const bloodRouter = express.Router();
 
 bloodRouter.post("", authenticateUser, uploadSome, postBlood); // 지정헌혈글 저장
 bloodRouter.patch("/:bloodId", authenticateUser, uploadSome, patchBlood); // 지정헌혈글 수정
-bloodRouter.get("/:page", authenticateUser, getAllBloods); // 지정헌혈글 전체 조회
+bloodRouter.get("/:page/all", authenticateUser, getAllBloods); // 지정헌혈글 전체 조회
 bloodRouter.get("/:bloodId", authenticateUser, getBlood); // 지정헌혈글 상세 조회
 bloodRouter.post("/:bloodId/like", authenticateUser, likeBlood); // 지정헌혈글 좋아요
 bloodRouter.patch("/:bloodId/like", authenticateUser, undoLikeBlood); // 지정헌혈글 좋아요 취소
