d3433f53c9b4877643ca17692d4483eb2c12a663
Jieun Kim <imfunctionx@gamil.com>
2024-02-18 10:43:48 +0900

    feat(#103): History API 작성

--- src/router/historyRouter.js
+++ src/router/historyRouter.js
@@ -6,6 +6,7 @@ import {
     updateHistory,
     deleteHistory,
     imageToText,
+    getAvailableDate,
 } from "../controller/historyController.js";
 import { authenticateUser } from "../middleware/authMiddleware.js";
 import { uploadSome } from "../middleware/imageMiddleware.js";
@@ -14,7 +15,8 @@ export const historyRouter = express.Router();
 
 historyRouter.post("", authenticateUser, postHistory); // 헌혈 증서 등록
 historyRouter.get("", authenticateUser, getAllHistories); // 헌혈 정보 전체 조회
-historyRouter.get("/:historyId", authenticateUser, getHistory); // 헌혈 정보 상세 조회
+historyRouter.get("/detail/:historyId", authenticateUser, getHistory); // 헌혈 정보 상세 조회
 historyRouter.patch("/:historyId", authenticateUser, updateHistory); // 헌혈 정보 수정
 historyRouter.delete("/:historyId", authenticateUser, deleteHistory); // 헌혈 정보 삭제
 historyRouter.post("/image", authenticateUser, uploadSome, imageToText); // 헌혈 증서 이미지 텍스트 추출
+historyRouter.get("/date", authenticateUser, getAvailableDate); // 전혈 가능 날짜 조회
